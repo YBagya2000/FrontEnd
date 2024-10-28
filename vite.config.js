0de1ffbb6d705ef9f7957e0ad2d6815bc8e13726
YBagya2000 <yasithbagya@gmail.com>
2024-10-29 03:35:57 +0530

    Day 5.2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port:80,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
